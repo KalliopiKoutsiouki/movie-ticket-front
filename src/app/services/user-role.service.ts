c8cfc0fad0bdb84edcf7585cc53deac026e7264d
Irene <irenebarbouni@gmail.com>
2024-04-23 22:32:12 +0300

    added forgotten jwt token to hall admin service calls

--- src/app/services/user-role.service.ts
+++ src/app/services/user-role.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../environment';
@@ -8,15 +8,30 @@ import { AuthService } from './auth.service';
   providedIn: 'root'
 })
 export class UserRoleService {
+  //todo: add token
 
   private baseUrl = environment.backendBaseUrl;
   constructor(private http:HttpClient, private authService: AuthService) { }
 
   addUserRole(userId: number, role: string): Observable<any> {
-    return this.http.put(`${this.baseUrl}/user/${userId}/role?role=${role}`, null);
+    const token = this.authService.getJwtToken();
+    const headerOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      })
+    };
+    return this.http.put(`${this.baseUrl}/user/${userId}/role?role=${role}`, null, headerOptions);
   }
 
   removeUserRole(userId: number, role: string): Observable<any> {
-    return this.http.delete(`${this.baseUrl}/user/${userId}/role?role=${role}`);
+    const token = this.authService.getJwtToken();
+    const headerOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      })
+    };
+    return this.http.delete(`${this.baseUrl}/user/${userId}/role?role=${role}`, headerOptions);
   }
 }
