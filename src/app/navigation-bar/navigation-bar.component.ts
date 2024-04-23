1a9607a3c28ecd1f52e24fe1dab8a2b4a1f0a309
Irene <irenebarbouni@gmail.com>
2024-04-23 23:31:54 +0300

    fixed admin tabs visibility

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { UserService } from '../services/user.service';
+import { User } from '../model/user';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -11,6 +12,7 @@ import { UserService } from '../services/user.service';
 export class NavigationBarComponent implements OnInit {
   user: any = null;
   isLoggedIn: boolean = false;
+  isAdmin: boolean = false;
   constructor(private router: Router, private authService: AuthService, private userService: UserService) { }
   isDropdownOpen: boolean = false;
 
@@ -19,6 +21,14 @@ export class NavigationBarComponent implements OnInit {
       this.isLoggedIn = loggedIn;
     });
     this.user = this.authService.getCurrentUser();
+    this.userService.getUserByUserName(this.user.username).subscribe(
+      (user: User) => {
+        this.isAdmin = user.userRoles.includes('ROLE_ADMIN') || user.userRoles.includes('ROLE_SUPERADMIN');
+      },
+      (error) => {
+        console.error('Error fetching user:', error);
+      }
+    );
     console.log(this.user);
   }
 
